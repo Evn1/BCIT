bfb1164c7fc9b394bb36cec7d4149f94141ccd55
Vantus555 <nindzay1998@gmail.com>
2019-10-24 09:29:55 +0300

    a == 0 || b ==0 || c == 0

--- Labs/Lab_1/Program.cs
+++ Labs/Lab_1/Program.cs
@@ -5,6 +5,7 @@ namespace Lab
     {
         static void Main(string[] args)
         {
+            double x1, x2, a, b, c;
             while (true)
             {
                 Console.WriteLine("Введите коэффициенты: ");
@@ -15,6 +16,35 @@ namespace Lab
                     Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine("Неверный формат данных\n");
                     Console.ResetColor();
+                }
+            }
+            if (a == 0 || b == 0 || c == 0)
+            {
+                if ((a == 0 && b == 0) || (-c / b < 0) || (-c / a < 0))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Действительных корней нет");
+                }
+                else if (((a == 0 || b == 0) && c == 0) || ((a != 0) && (-b / a < 0)))
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Уравнение имеет один корень: 0");
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    if ((a != 0) && (-c / a > 0))
+                    {
+                        Console.WriteLine($"Уравнение имеет два корня: {Math.Pow((-c / a), 0.25)} , {-(Math.Pow((-c / a), 0.25))}");
+                    }
+                    if ((b != 0) && (-c / b > 0))
+                    {
+                        Console.WriteLine($"Уравнение имеет два корня: {Math.Sqrt((-c / b))} , {-(Math.Sqrt(-c / b))}");
+                    }
+                    if ((a != 0) && (-b / a > 0))
+                    {
+                        Console.WriteLine($"Уравнение имеет три корня: 0 , {Math.Sqrt((-b / a))} , {-(Math.Sqrt(-b / a))}");
+                    }
                 }
             }
         }
