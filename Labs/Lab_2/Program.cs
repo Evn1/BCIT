5962c98444c29e5305cd0d711ebc2fa0e715e4ad
Evn1 <51531141+Evn1@users.noreply.github.com>
2019-11-06 21:37:52 +0300

    Test

---
+++ Labs/Lab_2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lab_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r1 = new Rectangle(5, 6);
+            var q1 = new Quadrate(10);
+            var c1 = new Circle(15);
+            r1.Print();
+            q1.Print();
+            c1.Print();
+        }
+    }
+}
