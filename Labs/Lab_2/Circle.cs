519782cad65d97e8fb491b0f17e330e89b455674
Evn1 <51531141+Evn1@users.noreply.github.com>
2019-11-06 21:34:11 +0300

    Created Circle class with  2 overrided methods Square and ToString, gemetric_figure is base class for Circle

---
+++ Labs/Lab_2/Circle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_2
+{
+    class Circle : Geometric_figure
+    {
+        public double radius { get; set; }
+        public Circle(double Radius)
+        {
+            radius = Radius;
+        }
+        public override double Square() => (Math.PI * radius * radius);
+        public override string ToString() => ($"~~~~CIRCLE~~~~\nradius: {radius}\nsquare: {Square()}\n");
+    }
+}
