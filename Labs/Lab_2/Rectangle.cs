d99fcc5dfb89dc125fad04d3f651688a86022975
Evn1 <51531141+Evn1@users.noreply.github.com>
2019-11-06 21:27:47 +0300

    Created Rectangle class with  2 overrided methods Square and ToString, gemetric_figure is base class for Rectangle

---
+++ Labs/Lab_2/Rectangle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_2
+{
+    class Rectangle : Geometric_figure
+    {
+        public double width { get; set; }
+        public double height { get; set; }
+        public Rectangle(double Width, double Height)
+        {
+            width = Width;
+            height = Height;
+        }
+        public override double Square() => (width * height);
+        public override string ToString() => ($"~~~~Rectangle~~~~\nwidth: {width}\nheight: {height}\nsquare: {Square()}\n");
+    }
+}
